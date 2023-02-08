785a6ede07d892e18086c3e2eb202b393d5856c6
Marc Regnier <marc.reg083@gmail.com>
2023-02-08 23:27:27 +0100

    progress bar slider modif

--- js/app.js
+++ js/app.js
@@ -1,22 +1,41 @@
-var slider = document.querySelector(".slider");
-console.log(slider);
-var slides = slider.children;
-var currentSlide = 0;
+const slider = document.querySelector(".slider");
+const progressBar = document.querySelector(".progress_bar");
+let slides = slider.children;
+let currentSlide = 0;
 
-var nextButton = document.querySelector(".next");
-var prevButton = document.querySelector(".prev");
+
+const nextButton = document.querySelector(".next");
+const prevButton = document.querySelector(".prev");
 
 function moveToNextSlide() {
   slides[currentSlide].classList.remove("current");
   currentSlide = (currentSlide + 1) % slides.length;
   slides[currentSlide].classList.add("current");
+  if (currentSlide) {
+    progressBar.value += (currentSlide / slides.length) * 100;
+  } else {
+    progressBar.value = (progressBar.max / slides.length);
+  }
+  
+  console.log(progressBar);
 }
 
 function moveToPrevSlide() {
   slides[currentSlide].classList.remove("current");
   currentSlide = (currentSlide - 1 + slides.length) % slides.length;
   slides[currentSlide].classList.add("current");
+  //progressBar.value -= (currentSlide / slides.length) * 100;
+  if (currentSlide) {
+    progressBar.value -= (currentSlide / slides.length) * 100;
+    console.log(progressBar.value);
+  } 
+  else{
+    progressBar.value = (progressBar.max / slides.length);
+    //console.log(progressBar.value);
+  }
 }
+
+
 
 nextButton.addEventListener("click", moveToNextSlide);
 prevButton.addEventListener("click", moveToPrevSlide);
