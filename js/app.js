d2c73d948e796049fc05c28caaf9a16f81bd9730
Marc Regnier <marc.reg083@gmail.com>
2023-02-09 19:14:55 +0100

    progress bar js works

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const slider = document.querySelector(".slider");
 const progressBar = document.querySelector(".progress_bar");
+const progressValue = 34
 let slides = slider.children;
 let currentSlide = 0;
-
 
 const nextButton = document.querySelector(".next");
 const prevButton = document.querySelector(".prev");
@@ -11,27 +11,26 @@ function moveToNextSlide() {
   slides[currentSlide].classList.remove("current");
   currentSlide = (currentSlide + 1) % slides.length;
   slides[currentSlide].classList.add("current");
+
   if (currentSlide) {
     progressBar.value += (currentSlide / slides.length) * 100;
   } else {
     progressBar.value = (progressBar.max / slides.length);
   }
-  
-  console.log(progressBar);
 }
 
 function moveToPrevSlide() {
   slides[currentSlide].classList.remove("current");
   currentSlide = (currentSlide - 1 + slides.length) % slides.length;
   slides[currentSlide].classList.add("current");
-  //progressBar.value -= (currentSlide / slides.length) * 100;
-  if (currentSlide) {
+
+  if (currentSlide && progressBar.value > progressValue) {
     progressBar.value -= (currentSlide / slides.length) * 100;
-    console.log(progressBar.value);
-  } 
-  else{
+  }
+  else if (currentSlide && progressBar.value < progressValue) {
+    progressBar.value += (currentSlide / slides.length) * 100;
+  } else {
     progressBar.value = (progressBar.max / slides.length);
-    //console.log(progressBar.value);
   }
 }
 
@@ -39,3 +38,4 @@ function moveToPrevSlide() {
 
 nextButton.addEventListener("click", moveToNextSlide);
 prevButton.addEventListener("click", moveToPrevSlide);
+
